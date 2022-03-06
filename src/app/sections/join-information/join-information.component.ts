b56d2d41693ae65f17169d098ccb34e63ea53858
이정훈 <58647036+Hulkdev600@users.noreply.github.com>
2022-03-07 08:57:01 +0900

    가입정보입력 컴포넌트 UPDATE

--- src/app/sections/join-information/join-information.component.ts
+++ src/app/sections/join-information/join-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,10 +8,18 @@ import { FormGroup } from '@angular/forms';
 })
 export class JoinInformationComponent implements OnInit {
 
+  @Output() changePage:EventEmitter<any> = new EventEmitter();
   userForm! : FormGroup
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  nextStep(){
+    this.changePage.emit(4);
+  }
+
+
 
 }

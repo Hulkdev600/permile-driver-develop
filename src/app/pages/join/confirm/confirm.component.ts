a491017f837dedbaebfa5c829091de0c952640bf
이정훈 <58647036+Hulkdev600@users.noreply.github.com>
2022-04-24 11:12:20 +0900

    안드로이드 backKey 클릭 시 모달 닫기를 위한 작업

--- src/app/pages/join/confirm/confirm.component.ts
+++ src/app/pages/join/confirm/confirm.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit,Output, EventEmitter, Input,ViewChild,TemplateRef, ElementRef } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import { PlatformLocation } from '@angular/common'
+
 import { DomSanitizer} from '@angular/platform-browser';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'page-confirm',
@@ -11,15 +16,19 @@ import { DomSanitizer} from '@angular/platform-browser';
 })
 
 export class ConfirmComponent implements OnInit {
-
+  modalRef: NgbModalRef | undefined;
   @Input() payload:any
   @Output("changePage") changePage:EventEmitter<any> = new EventEmitter();
+  @Output("moveBack") moveBack:EventEmitter<any> = new EventEmitter();
   /** 모달 View **/
   @ViewChild('kakaoPaymentAppModal') kakaoPaymentAppModal: TemplateRef<any> | undefined
   @ViewChild('contractSuccessModal') contractSuccessModal: TemplateRef<any> | undefined
   @ViewChild('contractFailModal') contractFailModal: TemplateRef<any> | undefined
   @ViewChild('iframe') iframe: ElementRef<any> | undefined
   
+  faCircleCheck = faCircleCheck;
+  faCircleExclamation = faCircleExclamation
+
   getProductEndpoint : string ='product'
   productName:string ='';
   productType:string ='';
@@ -38,14 +47,40 @@ export class ConfirmComponent implements OnInit {
     private _httpService : HttpService,
     private activatedRoute : ActivatedRoute,
     private modalService: NgbModal,
-  ) { }
+    private location: PlatformLocation
+  ) { 
+    location.onPopState((event)=>{
+      console.log(this.modalRef)
+      if(this.modalRef !== undefined) {
+        this.modalRef.close();
+        // this.modalService.dismissAll()
+        console.log('colse bmodal')
+        return
+      }
+      if(this.modalRef == undefined){
+        console.log('emit page')
+        
+        this.moveBack.emit();
+      }
+      
+
+
+    })
+  }
 
   ngOnInit(): void {
+    history.pushState(null, '', 'page3');
     console.log(this.payload)
     this.getUser()
 
     this.getProduct()
+    
   }
+
+  ngOnDestroy() {
+    this.modalService.dismissAll()
+  }
+
 
   private getUser(){
 
@@ -113,78 +148,157 @@ export class ConfirmComponent implements OnInit {
 
     console.log(varUA);
 
+
+    // const modalState = {
+    //   modal : true,
+    //   desc : 'fake state for our modal'
+    // };
+    // history.pushState(modalState, '');
+
+    // history.pushState(null, 'modalOpened');
     /**
      * 
      * 결제창 띄우기
      * **/
-    this.appSchemeURL ="https://www.youtube.com/embed/ZNX4BnD2_9Q"
+    // this.appSchemeURL ="https://www.youtube.com/embed/ZNX4BnD2_9Q"
+    this.appSchemeURL = `webview://payment?action=select_payment&item_id=${leafletId}id&vertical_code=CAR_INS&action_type=create`
     
-    setTimeout(() => {
-      this.open(this.kakaoPaymentAppModal,'my-class-kakao-payment-app-modal');
-      // this.open(this.contractSuccessModal,'my-class-kakao-payment-app-modal');
-      this.payAppOnOff = true;
-    },3000)
+
+    this.open(this.kakaoPaymentAppModal,'my-class-kakao-payment-app-modal');
+    // this.open(this.contractSuccessModal,'my-class-kakao-payment-app-modal');
     
-    // this.appSchemeURL = `webview://payment?action=select_payment&item_id=${leafletId}id&vertical_code=CAR_INS&action_type=create`
+    this.payAppOnOff = true;
+    
+
+    
+    
     
   }
 
+  /**
+   * 계약요청보내기
+   * 
+   */
   contract(){
+    
     this._httpService.sendPostRequest(this.contractURLEndpoint, this.payload).subscribe((response:any)=> {
-      // alert(response.message);
       
-      let contractStatus = response.status
-      let contractMessage = response.body.message
-      let openModal = contractStatus === 200 ? this.contractSuccessModal : this.contractFailModal;
-      let openModalClass = contractStatus === 200 ? 'my-class-contract-success-modal' : 'my-class-contract-fail-modal';
-      
-      this.contractMessage = contractMessage      
-      this.open(openModal, openModalClass)  
+      // console.log(response)
+      // console.log(response.body)
+      // console.log(response)
+      // 먼저 결제모달을 내린다.
+      this.modalService.dismissAll();
 
-    })
+
+      let resultCode = response.code
+      let resultMessage = response.message
+      let modal = resultCode === '200' ? this.contractSuccessModal : this.contractFailModal;
+      let modalClass = resultCode === '200' ? 'my-class-contract-success-modal' : 'my-class-contract-fail-modal';
+      this.contractMessage = resultMessage      
+      
+      this.open(modal, modalClass)  
+
+    }),
+    (errResponse:any)=>{
+      console.log(errResponse)
+      // this.open()
+      alert('네트워크 문제가 발생하였습니다. 잠시 후에 다시 시도해주시기 바랍니다')
+    }
   }
+  
   onPaymentDialogSuccess(itemId: string){
-    alert('T_결제성공 Alert_contract 메소드 실행')
+    
     this.contract()
   }
+
   onPaymentDialogFail(itemId: any, errorCode: String, message: String){
     alert('T_결제수단 등록 실패 :: ' + message)    
+
   }
+
+  //결제창이 닫혔을 때 호출하는 페이지
   onPaymentDialogClose(){
-    alert('닫기.')
+    // alert('닫기.')
   }
 
   
 
 
-  open(content:any,myClass?:string) {
-    // console.log(content)
+  /**
+   * Modal 열기 
+   * @param content 
+   * @param myClass 
+   */
+  // open(content:any,myClass?:string):void {
+
+  //   let ngbModalOption:any = {ariaLabelledBy: 'modal-basic-title'}
+  //   if(myClass){
+  //     ngbModalOption['windowClass'] = myClass
+  //   }
+
+  //   history.pushState(null, '', 'modalOpened');
+
+
+  //   this.modalService.open(content, ngbModalOption).result.then((result:any) => {
+  //     // console.log('모달 result : ',result)
+  //     this.closeResult = `Closed with: ${result}`;
+  //   }, (reason:any) => {
+  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  //   });
+  // }
+
+  // private getDismissReason(reason: any): string {
+
+    
+
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return `with: ${reason}`;
+  //   }
+  // }
+
+
+
+
+  open(content:any,myClass?:string):void {
+
     let ngbModalOption:any = {ariaLabelledBy: 'modal-basic-title'}
     if(myClass){
       ngbModalOption['windowClass'] = myClass
     }
 
-    this.modalService.open(content, ngbModalOption).result.then((result:any) => {
-      // console.log('모달 result : ',result)
+    history.pushState(null, '', 'modalOpened');
+
+
+    this.modalRef = this.modalService.open(content,ngbModalOption);
+
+     // push new state to history
+     history.pushState(null, '', 'modalOpened');
+
+     this.modalRef.result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
-    }, (reason:any) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+
   }
-
-
 
   private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
+      // go back in history if the modal is closed normal (ESC, backdrop click, cross click, close click)
+      history.back();
+    // history.go(1); // 이거로 하면 이상함
+    
+      if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+      } else {
+          return  `with: ${reason}`;
+      }
+  }  
+  
 
-  closeTest(){
-    this.modalService.dismissAll()
-  }
 }
